207dc273b7ebe6a97873c546b2f91a69b00fea55
Olivier Boucher <olivier@bouchernolet.com>
2016-03-26 16:36:34 -0400

    initial commit - npm and webpack configs

---
+++ webpack-production.config.js
@@ -0,0 +1,58 @@
+/**
+ * Created by olivier on 2016-03-26.
+ */
+
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: [
+        path.resolve(__dirname, './src/main.js')
+    ],
+
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        })
+    ],
+
+    modules: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel',
+                query: {
+                    presets : ['es2015', 'react', 'stage-0']
+                },
+                exclude : path.join(__dirname, './node_modules/')
+            },
+            {
+                test: /\.scss$/,
+                loaders: ['style', 'css?modules', 'sass'],
+                include: path.resolve(__dirname, './node_modules/react-flexbox-grid')
+            },
+            { test: /\.css$/, loader: 'style!css'},
+            { test: /\.scss$/, loader: 'style!css!sass'},
+            { test: /\.less$/, loader: 'style!css!less'},
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
+            { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
+            { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' }
+        ]
+    }
+};
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+/**
+ * Created by olivier on 2016-03-26.
+ */
+
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: [
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/only-dev-server',
+        path.resolve(__dirname, './src/main.js')
+    ],
+
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+
+    modules: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'react-hot',
+                exclude : path.join(__dirname, './node_modules/')
+            },
+            {
+                test: /\.jsx?$/,
+                loader: 'babel',
+                query: {
+                    presets : ['es2015', 'react', 'stage-0']
+                },
+                exclude : path.join(__dirname, './node_modules/')
+            },
+            {
+                test: /\.scss$/,
+                loaders: ['style', 'css?modules', 'sass'],
+                include: path.resolve(__dirname, './node_modules/react-flexbox-grid')
+            },
+            { test: /\.css$/, loader: 'style!css'},
+            { test: /\.scss$/, loader: 'style!css!sass'},
+            { test: /\.less$/, loader: 'style!css!less'},
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
+            { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
+            { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' }
+        ]
+    }
+};
